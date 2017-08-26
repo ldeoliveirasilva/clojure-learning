d005a48428ef7f9d601bccb48ccd34a9d6f4d8fb
Leonardo Silva <ldeoliveirasilva.dev@gmail.com>
2017-08-26 15:17:20 -0300

    add lost and game functions

--- forca/src/forca/core.clj
+++ forca/src/forca/core.clj
@@ -1,7 +1,19 @@
 (ns forca.core
   (:gen-class))
 
-(def total-de-vidas 6)
+(def total-of-lives 6)
+
+(defn lost [] (print "You lost!"))
+
+(defn game [lives] 
+	(if (= lives 0)
+		(lost)
+		(do 
+			(print lives)
+			(game (dec 1))	
+		)
+	)
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
