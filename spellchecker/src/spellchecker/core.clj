2ea2df5310f2ff2433580637d30fb6aee9e81300
Leonardo Silva <ldeoliveirasilva.dev@gmail.com>
2017-08-30 15:12:28 -0300

    add function to check if a word is correct

--- spellchecker/src/spellchecker/core.clj
+++ spellchecker/src/spellchecker/core.clj
@@ -1,5 +1,16 @@
 (ns spellchecker.core
-  (:gen-class))
+  (:require [clojure.string :as str]))
+
+(def words-file-path "resources/wordsEn.txt")
+
+(def words
+	(->> 
+		(slurp words-file-path)
+		(str/split-lines)
+		(map str/trim)
+		(set)))
+
+(defn correct? [word] (contains? words word))
 
 (defn -main
   "I don't do a whole lot ... yet."
